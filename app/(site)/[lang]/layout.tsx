ef127f3ec2651ddb46e52b90b75045ddf894d8bd
Sander de snaijer <sanderdesnaijer@gmail.com>
2026-01-11 17:01:42 +0700

    feat: google analytics

--- app/(site)/[lang]/layout.tsx
+++ app/(site)/[lang]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../../globals.css";
 import { Lang } from "../../i18n";
+import GoogleAnalytics from "@/app/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,9 @@ export default async function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleAnalytics ga_id={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
         {children}
       </body>
     </html>
